9a507f02afc59898157afae19265ceaa1387ad64
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-03-14 13:52:35 +0300

    Edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'; 
+newHeader.textContent = "Lawrence Wambugu is the champion";
+
+document.body.appendChild(newHeader);
